e3aafaa891a39c2ffcde3831bc9b6489ca472986
TinaHeiligers <tina.heiligers@gmail.com>
2017-04-17 18:19:41 -0400

    working on products tests

--- server/products.test.js
+++ server/products.test.js
@@ -9,6 +9,14 @@ describe('/api/products', () => {
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
+  describe('GET /', () =>
+    describe('when there are no products', () =>
+      it('fails with a 403 (Not found)', () =>
+        request(app)
+          .get(`/api/products`)
+          .expect(403)
+      )))
+
   describe('GET /:id', () =>
     describe('when product does not exist', () =>
       it('fails with a 403 (Not found)', () =>
@@ -41,4 +49,60 @@ describe('/api/products', () => {
             price: 12
           })))
     }))
+
+  describe('PUT', () =>
+    describe('only admin can edit a product', () => {
+      it('updates a product', () =>
+        request(app)
+          .put('/api/products/1')
+          .send({
+            name: 'new booties',
+            price: 24
+          })
+          .expect(200))
+
+      it('redirects to the product it just made', () =>
+        request(app)
+          .put('/api/products/1')
+          .send({
+            name: 'new booties',
+            price: 24
+          })
+          .redirects(1)
+          .then(res => expect(res.body).to.contain({
+            name: 'new booties',
+            price: 24
+          })))
+    }))
+
+  // describe('DELETE', () =>
+  //   describe('only admin can delete a product', () => {
+  //     before('Creates a product', () =>
+  //       request(app)
+  //         .post('/api/products')
+  //         .send({
+  //           name: 'deleteMe',
+  //           price: 500
+  //         }))
+  //     it('deletes a product', () =>
+  //       request(app)
+  //         .delete('/api/products')
+  //         .send({
+  //           name: 'booties',
+  //           price: 12
+  //         })
+  //         .expect(201))
 })
+// TODOS-tests
+// GET
+// X Find all products
+// X Find products by ID
+
+// POST
+// X Add a product
+
+// UPDATE
+// X admin: update product
+
+// DELETE
+// admins: delete a product
