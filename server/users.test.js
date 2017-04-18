4f9d03abcb8c27536983b7f49d279620128491fb
Maria Schreiber <meschreiber3@gmail.com>
2017-04-18 12:32:47 -0400

    user put test functioning and passed

--- server/users.test.js
+++ server/users.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest')
-    , {expect} = require('chai')
-    , db = require('APP/db')
-    , app = require('./start')
+  , { expect } = require('chai')
+  , db = require('APP/db')
+  , app = require('./start')
+  , User = db.model('users')
 
 /* global describe it before afterEach */
 
-describe('/api/users', () => {
+describe.only('/api/users', () => {
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
@@ -17,27 +18,64 @@ describe('/api/users', () => {
           .expect(401)
       )))
 
-  describe('POST', () =>
-    describe('when not logged in', () => {
-      it('creates a user', () =>
-        request(app)
-          .post('/api/users')
-          .send({
-            email: 'beth@secrets.org',
-            password: '12345'
-          })
-          .expect(201))
+  describe('PUT /:id', () => {
+    let id
 
-      it('redirects to the user it just made', () =>
-        request(app)
-          .post('/api/users')
-          .send({
-            email: 'eve@interloper.com',
-            password: '23456',
-          })
-          .redirects(1)
-          .then(res => expect(res.body).to.contain({
-            email: 'eve@interloper.com'
-          })))
-    }))
+    before(function() {
+      return User.create({
+        email: 'wasssupp@gmail.com',
+        password: '12345'
+      })
+        .then(user => {
+          console.log('we made the user', user.id)
+          id = user.id
+        })
+        .catch(console.error)
+    })
+
+
+    it('updates a user', () => {
+      // Why does this complete before the 'before' block completes?
+      console.log('Were searching for user', id)
+      request(app)
+        .put(`/api/users/${id}`)
+        .send({
+          email: 'testest@test.com',
+        })
+        .expect(201)
+        .then(res => {
+          return User.findById(id)
+        })
+        .then(user => {
+          expect(user.email).to.be.equal('testest@test.com')
+        })
+        .catch(console.error)
+    }
+
+    )
+
+    describe('POST', () =>
+      describe('when not logged in', () => {
+        it('creates a user', () =>
+          request(app)
+            .post('/api/users')
+            .send({
+              email: 'beth@secrets.org',
+              password: '12345'
+            })
+            .expect(201))
+
+        it('redirects to the user it just made', () =>
+          request(app)
+            .post('/api/users')
+            .send({
+              email: 'eve@interloper.com',
+              password: '23456',
+            })
+            .redirects(1)
+            .then(res => expect(res.body).to.contain({
+              email: 'eve@interloper.com'
+            })))
+      }))
+  })
 })
